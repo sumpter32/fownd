8dac785f6c33febce9cddead349c70fd131b493f
Steven S <sumpter32@gmail.com>
2023-04-06 12:43:01 +0800

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,22 +14,4 @@ import UserNotifications
     GMSServices.provideAPIKey("AIzaSyCUwR60JXqQWI5nX1t_oxZyDx-GQxVSgKw")
     FirebaseApp.configure() //add this before the code below
     
-    // Push notifications configuration
-    if #available(iOS 10.0, *) {
-        UNUserNotificationCenter.current().delegate = self
-    }
-    
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-  
-  @available(iOS 10.0, *)
-  override func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-      completionHandler([.banner, .badge, .sound])
-  }
-
-  @available(iOS 10.0, *)
-  override func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-      completionHandler()
-  }
 }
